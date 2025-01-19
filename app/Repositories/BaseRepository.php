15a9587ec11077b69ba61ca62551d444dd612e98
Luiz Miguel <luizmiguel277@gmail.com>
2025-01-19 14:57:58 -0300

    feat: create transactions

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repositories;
+
+abstract class BaseRepository
+{
+
+    protected $modelClass;
+
+    protected function newQuery()
+    {
+        return app($this->modelClass)->newQuery();
+    }
+
+    protected function doQuery($query = null, $take = 15, $paginate = true)
+    {
+        if (is_null($query)) {
+            $query = $this->newQuery();
+        }
+
+        if ($paginate == true) {
+            return $query->paginate($take);
+        }
+
+        if ($take > 0 || $take !== false) {
+            $query->take($take);
+        }
+
+        return $query->get();
+    }
+
+    public function getAll($take = 15, $paginate = true)
+    {
+        return $this->doQuery(null, $take, $paginate);
+    }
+
+    public function lists($column, $key = null)
+    {
+        return $this->newQuery()->lists($column, $key);
+    }
+
+    public function findById($id, $fail = true)
+    {
+        if ($fail) {
+            return $this->newQuery()->findOrFail($id);
+        }
+
+        return $this->newQuery()->find($id);
+    }
+
+    public function findByAttribute(string $attribute, $value)
+    {
+        return $this->newQuery()->where($attribute, $value)->get();
+    }
+}
